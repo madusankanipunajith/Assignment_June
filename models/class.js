1e6343de10a6ec0de9d7f5a3ffde1b5276c457d9
Harshana Walpita <harshanawalpita.com>
2022-06-18 12:10:14 +0530

    enum

--- models/class.js
+++ models/class.js
@@ -1,22 +1,22 @@
-let class_module = require('../data/class.json');
+let classModule = require('../data/class.json');
 const filename = './data/class.json';
 const helper = require('../helpers/helper');
 
 function insertClass(newClassModule) {
     return new Promise((resolve, reject) => {
 
-        const id = helper.getNewId(class_module)
+        const id = helper.getNewId(classModule)
         const date = { 
             createdAt: helper.newDate(),
             updatedAt: helper.newDate()
-        } 
+        }
 
         newClassModule = {...newClassModule,
                     id: id,
                     date: date }
 
-        class_module.push(newClassModule)
-        helper.writeJSONFile(filename, class_module)
+        classModule.push(newClassModule)
+        helper.writeJSONFile(filename, classModule)
         resolve(newClassModule).catch(err => reject(err))
     })
 }
