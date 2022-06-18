281dff7652fde16cf11a3e849f8ae75483baf481
Harshana Walpita <harshanawalpita.com>
2022-06-18 11:48:28 +0530

    enum, student

--- controllers/instructor/private.js
+++ controllers/instructor/private.js
@@ -3,9 +3,9 @@ const enums = require("../../utils/enums");
 
 exports.insertClassModule = async(req, res, next) =>{
     try {
-        const {name} = req.body;
-        const password = Math.floor((Math.random() * 1000000) + 10000);
-        const classModule = await ClassModule.insertClass({name, password, type: 2});
+        const {module_enum} = req.body;
+        const student = req.body;
+        const classModule = await ClassModule.insertClass({module_enum, student});
         
         return res.status(200).json({
             success: true,
